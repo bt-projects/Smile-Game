1d6065a41328f5e82743e3ff68158a7b83dc0d9d
Bishow Thapa <bishowthapa33@gmail.com>
2022-11-15 00:46:39 +0545

    Link app to project settings

--- smile/login/views.py
+++ smile/login/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    # template = loader.get_template('')
+    context = {
+        # 'name': bishow,
+    }
+
+    return render(request, 'login/index.html')
