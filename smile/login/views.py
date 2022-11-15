1f656ad1e9b9258b5ade151016becece07ff9a33
Bishow Thapa <bishowthapa33@gmail.com>
2022-11-15 13:07:31 +0545

    Demo Database Integration Working

--- smile/login/views.py
+++ smile/login/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import DemoSmile
 
 def index(request):
     # template = loader.get_template('')
+    smile = DemoSmile.objects.all()
     context = {
-        # 'name': bishow,
+        'smile': smile
     }
 
-    return render(request, 'login/index.html')
+    return render(request, 'login/index.html', context)
