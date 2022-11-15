4877204da1ec5d144e5027f9ac18ffd2f4cc5e47
Bishow Thapa <bishowthapa33@gmail.com>
2022-11-15 14:27:41 +0545

    Signup and Login Completed

--- smile/login/views.py
+++ smile/login/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
 from .models import DemoSmile
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 
 def index(request):
     # template = loader.get_template('')
@@ -10,4 +12,23 @@ def index(request):
         'smile': smile
     }
 
-    return render(request, 'login/index.html', context)
+    if request.method == 'POST':
+        # email = request.POST['email']
+        username = request.POST['username']
+        password = request.POST['password']
+
+        # user = auth.authenticate(email=email, password=password)
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            messages.info(request, 'sucessfull login')
+            return redirect("/")
+        else:
+            messages.info(request, 'invalid credentails')
+            return redirect("/")
+    else:
+        # return render(request, 'login/index.html')
+        return render(request, 'login/index.html')
+
+    # return render(request, 'login/index.html', context)
