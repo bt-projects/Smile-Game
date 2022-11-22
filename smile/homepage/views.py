0bb738153410b0e4ba4681875e91b036eaad3996
Bishow Thapa <bishowthapa33@gmail.com>
2022-11-22 17:25:29 +0545

    My Second Release - API Integration

--- smile/homepage/views.py
+++ smile/homepage/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from urllib.request import urlopen
+import json
 
 def home(request):
-    context = {
-        
-    }
-    # return HttpResponse("This is home page")
-    return render(request, 'homepage/index.html')
+    # store the URL in url as parameter for urlopen
+    url = "https://marcconrad.com/uob/smile/api.php"
+    
+    # store the response of URL
+    response = urlopen(url)
+    
+    # storing the JSON response from url in data
+    smileJson = json.loads(response.read())
+
+    # context = {
+    #     'smileUrl': 'https://marcconrad.com/uob/smile/api.php',
+    #      'githubAPI': 'https://api.github.com/',
+    # }
+
+    # my_context = data_json
+
+    # return render(request, 'homepage/index.html', smileJson)
+    return render(request, 'homepage/index.html', {'smileJson' : smileJson})
+    # return print(data_json)
