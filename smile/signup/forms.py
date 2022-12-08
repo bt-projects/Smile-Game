87160a4a593bae10b7b8f72d95ca8d07fc79f525
Bishow Thapa <bishowthapa33@gmail.com>
2022-12-08 22:21:44 +0545

    Oauth Updated UI

--- smile/signup/forms.py
+++ smile/signup/forms.py
@@ -6,5 +6,5 @@ class SignupForm(forms.Form):
     last_name = forms.CharField(max_length=150)
     username = forms.CharField(max_length=150)
     email = forms.CharField(max_length=254, validators=[validators.EmailValidator(message="Invalid Email")])
-    password1 = forms.CharField(max_length=128)
-    password2 = forms.CharField(max_length=128)
+    password1 = forms.CharField(max_length=128, min_length=5)
+    password2 = forms.CharField(max_length=128, min_length=5)
