dce4e2851749068c43173859cb8fa04791691347
Bishow Thapa <bishowthapa33@gmail.com>
2024-06-18 14:00:42 +0545

    updated the settings.py

--- smile/smile/settings.py
+++ smile/smile/settings.py
@@ -140,9 +140,11 @@ USE_TZ = True
 ####################################################################################
 # Email Settings
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+# Store your gmail
 EMAIL_HOST = os.environ.get('EMAIL_HOST')
 EMAIL_FROM = os.environ.get('EMAIL_FROM')
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+# Store your App generated password here
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = os.environ.get('EMAIL_PORT')
 EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS')
