a6694290f2c77c4d24fb624f853684c73b0d5fdf
Bishow Thapa <bishowthapa33@gmail.com>
2024-06-19 19:40:59 +0200

    updated the settings.py

--- smile/smile/settings.py
+++ smile/smile/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False').lower() in ('true', '1', 't')
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
